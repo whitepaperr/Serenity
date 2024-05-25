909546a83b2d88cf41635b7513d3c17dd2f3a962
haeun <haeuni437@gmail.com>
2024-05-24 17:49:49 -0700

    initial

---
+++ Serenity/Serenity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Serenity
+//
+//  Created by 이하은 on 5/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
