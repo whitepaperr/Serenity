4a3e2484301dce786a29211e6c70f7e9210519cb
haeun <haeuni437@gmail.com>
2024-05-25 20:02:54 -0700

    login/signup

---
+++ Serenity/Serenity/SignUpViewController.swift
@@ -0,0 +1,162 @@
+//
+//  SignUpViewController.swift
+//  Serenity
+//
+//  Created by 이하은 on 5/25/24.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+
+    let idTextField = UITextField()
+    let passwordTextField = UITextField()
+    let passwordConfirmTextField = UITextField()
+    let nameTextField = UITextField()
+    let birthDateTextField = UITextField()
+    let genderTextField = UITextField()
+    let signUpButton = UIButton()
+
+    let genderPicker = UIPickerView()
+    let genders = ["Male", "Female", "Transgender", "Non-binary", "Prefer Not to Answer"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.988, alpha: 1.0)
+
+        setupTextField(idTextField, placeholder: "Email")
+        setupTextField(passwordTextField, placeholder: "Password", isSecure: true, content: .newPassword)
+        setupTextField(passwordConfirmTextField, placeholder: "Confirm Password", isSecure: true, content: .newPassword)
+        setupTextField(nameTextField, placeholder: "Full Name")
+        setupTextField(birthDateTextField, placeholder: "Birth Date (DD/MM/YYYY)")
+        setupTextField(genderTextField, placeholder: "Gender")
+        genderTextField.delegate = self
+        setupPicker()
+        genderTextField.addTarget(self, action: #selector(tapGenderField), for: .touchDown)
+
+        signUpButton.setTitle("Sign Up", for: .normal)
+        signUpButton.backgroundColor = UIColor(red: 0.796, green: 0.764, blue: 0.890, alpha: 1.0)
+        signUpButton.layer.cornerRadius = 5
+        signUpButton.isEnabled = false
+        signUpButton.addTarget(self, action: #selector(signUpAction), for: .touchUpInside)
+        view.addSubview(signUpButton)
+
+        [idTextField, passwordTextField, passwordConfirmTextField, nameTextField, birthDateTextField, genderTextField].forEach {
+            $0.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        }
+    }
+
+    private func setupTextField(_ textField: UITextField, placeholder: String, isSecure: Bool = false, content: UITextContentType? = nil) {
+        textField.placeholder = placeholder
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = isSecure
+        textField.textContentType = content
+        textField.autocapitalizationType = .none
+        view.addSubview(textField)
+    }
+
+    private func setupPicker() {
+        genderPicker.delegate = self
+        genderPicker.dataSource = self
+        genderTextField.inputView = genderPicker
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissPicker))
+        toolbar.setItems([doneButton], animated: true)
+        genderTextField.inputAccessoryView = toolbar
+    }
+
+    private func setupConstraints() {
+        idTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordConfirmTextField.translatesAutoresizingMaskIntoConstraints = false
+        nameTextField.translatesAutoresizingMaskIntoConstraints = false
+        birthDateTextField.translatesAutoresizingMaskIntoConstraints = false
+        genderTextField.translatesAutoresizingMaskIntoConstraints = false
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            idTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            idTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            idTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            idTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            passwordTextField.topAnchor.constraint(equalTo: idTextField.bottomAnchor, constant: 20),
+            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordTextField.widthAnchor.constraint(equalTo: idTextField.widthAnchor),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            passwordConfirmTextField.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+            passwordConfirmTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordConfirmTextField.widthAnchor.constraint(equalTo: idTextField.widthAnchor),
+            passwordConfirmTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            nameTextField.topAnchor.constraint(equalTo: passwordConfirmTextField.bottomAnchor, constant: 20),
+            nameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nameTextField.widthAnchor.constraint(equalTo: idTextField.widthAnchor),
+            nameTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            birthDateTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 20),
+            birthDateTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            birthDateTextField.widthAnchor.constraint(equalTo: idTextField.widthAnchor),
+            birthDateTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            genderTextField.topAnchor.constraint(equalTo: birthDateTextField.bottomAnchor, constant: 20),
+            genderTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            genderTextField.widthAnchor.constraint(equalTo: idTextField.widthAnchor),
+            genderTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            signUpButton.topAnchor.constraint(equalTo: genderTextField.bottomAnchor, constant: 20),
+            signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signUpButton.widthAnchor.constraint(equalTo: idTextField.widthAnchor),
+            signUpButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+
+    @objc private func signUpAction() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    @objc private func editingChanged(_ textField: UITextField) {
+        let allFieldsFilled = !(idTextField.text?.isEmpty ?? true) &&
+                              !(passwordTextField.text?.isEmpty ?? true) &&
+                              !(passwordConfirmTextField.text?.isEmpty ?? true) &&
+                              !(nameTextField.text?.isEmpty ?? true) &&
+                              !(birthDateTextField.text?.isEmpty ?? true) &&
+                              !(genderTextField.text?.isEmpty ?? true)
+        signUpButton.isEnabled = allFieldsFilled
+        signUpButton.backgroundColor = allFieldsFilled ? UIColor(red: 0.758, green: 0.694, blue: 0.882, alpha: 1.0) : UIColor(red: 0.796, green: 0.764, blue: 0.890, alpha: 1.0)
+    }
+
+    @objc private func tapGenderField() {
+        genderTextField.becomeFirstResponder()
+    }
+
+    @objc private func dismissPicker() {
+        view.endEditing(true)
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return genders.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return genders[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        genderTextField.text = genders[row]
+        dismissPicker()
+        editingChanged(genderTextField)
+    }
+}
--- Serenity/Serenity/ViewController.swift
+++ Serenity/Serenity/ViewController.swift
@@ -9,11 +9,105 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let logoImageView = UIImageView()
+    let idTextField = UITextField()
+    let passwordTextField = UITextField()
+    let loginButton = UIButton()
+    let signUpTransitionButton = UIButton()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
     }
 
+    private func setupViews() {
+        view.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.988, alpha: 1.0)
 
+        logoImageView.image = UIImage(named: "Serenity")
+        logoImageView.contentMode = .scaleAspectFit
+        view.addSubview(logoImageView)
+
+        setupTextField(idTextField, placeholder: "Email")
+        setupTextField(passwordTextField, placeholder: "Password", isSecure: true)
+
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.backgroundColor = UIColor(red: 0.796, green: 0.764, blue: 0.890, alpha: 1.0)
+        loginButton.layer.cornerRadius = 5
+        loginButton.addTarget(self, action: #selector(loginAction), for: .touchUpInside)
+        loginButton.isEnabled = false
+        view.addSubview(loginButton)
+
+        let signUpText = NSMutableAttributedString(string: "You don't have an account? Then, ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13)])
+        signUpText.append(NSAttributedString(string: "Sign Up", attributes: [
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 13),
+            NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
+        ]))
+        signUpTransitionButton.setAttributedTitle(signUpText, for: .normal)
+        signUpTransitionButton.setTitleColor(.blue, for: .normal)
+        signUpTransitionButton.addTarget(self, action: #selector(showSignUp), for: .touchUpInside)
+        view.addSubview(signUpTransitionButton)
+
+        [idTextField, passwordTextField].forEach {
+            $0.addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        }
+    }
+
+    private func setupTextField(_ textField: UITextField, placeholder: String, isSecure: Bool = false) {
+        textField.placeholder = placeholder
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = isSecure
+        textField.autocapitalizationType = .none
+        textField.keyboardType = .emailAddress
+        view.addSubview(textField)
+    }
+
+    private func setupConstraints() {
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        idTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        signUpTransitionButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 150),
+            logoImageView.heightAnchor.constraint(equalToConstant: 150),
+
+            idTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 30),
+            idTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            idTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            idTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            passwordTextField.topAnchor.constraint(equalTo: idTextField.bottomAnchor, constant: 20),
+            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordTextField.widthAnchor.constraint(equalTo: idTextField.widthAnchor),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.widthAnchor.constraint(equalTo: idTextField.widthAnchor),
+            loginButton.heightAnchor.constraint(equalToConstant: 50),
+
+            signUpTransitionButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 10),
+            signUpTransitionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+
+    @objc private func loginAction() {
+        // Handle login logic here
+    }
+
+    @objc private func showSignUp() {
+        let signUpVC = SignUpViewController()
+        signUpVC.modalPresentationStyle = .fullScreen
+        present(signUpVC, animated: true, completion: nil)
+    }
+
+    @objc private func editingChanged(_ textField: UITextField) {
+        loginButton.isEnabled = !(idTextField.text?.isEmpty ?? true) && !(passwordTextField.text?.isEmpty ?? true)
+        loginButton.backgroundColor = loginButton.isEnabled ? UIColor(red: 0.758, green: 0.694, blue: 0.882, alpha: 1.0) : UIColor(red: 0.796, green: 0.764, blue: 0.890, alpha: 1.0)
+    }
 }
-
